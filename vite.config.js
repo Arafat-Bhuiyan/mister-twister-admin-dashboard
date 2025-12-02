2eac778f69ccd3f257bb6a91ded645b583bf8eba
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2025-12-02 18:37:18 +0600

    server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // your local IP (no leading space)
+    port: 5186,          // default Vite dev server port
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
